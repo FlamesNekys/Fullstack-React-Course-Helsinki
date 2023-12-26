dee6a6d280627e24ad2ca661283abcf8e01fcbb5
FlamesNekkys <yodacj21@gmail.com>
2023-12-26 20:47:20 +0300

    add 5.12

--- part5/bloglist-frontend/src/components/BlogForm.jsx
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
     const [title, setTitle] = useState('')
@@ -52,6 +53,10 @@ const BlogForm = ({ createBlog }) => {
             <button type="submit">Create</button>
         </form>
     )
+}
+
+BlogForm.propTypes = {
+    createBlog: PropTypes.func.isRequired,
 }
 
 export default BlogForm
