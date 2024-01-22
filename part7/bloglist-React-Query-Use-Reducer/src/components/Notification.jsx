8ba37b48ff97e3e52a35955e78974c583eb5ca9f
FlamesNekkys <yodacj21@gmail.com>
2024-01-22 21:45:53 +0300

    deleted unused variables

--- part7/bloglist-React-Query-Use-Reducer/src/components/Notification.jsx
+++ part7/bloglist-React-Query-Use-Reducer/src/components/Notification.jsx
@@ -6,28 +6,6 @@ const Notification = () => {
 
     if (!notification) return null
 
-    const messageStyle = {
-        border: 'green 2px solid',
-        backgroundColor: 'lightgrey',
-        color: 'green',
-        fontSize: 20,
-        height: 50,
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: 15,
-    }
-
-    const errorStyle = {
-        border: 'red 2px solid',
-        backgroundColor: 'lightgrey',
-        color: 'red',
-        fontSize: 20,
-        height: 50,
-        display: 'flex',
-        alignItems: 'center',
-        marginBottom: 15,
-    }
-
     if (notification.message)
         return (
             <div className="bg-violet-800 px-3 py-2 border-2 border-emerald-600 w-screen shadow-xl animate-appear">
